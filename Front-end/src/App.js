8d30292ff77532175f1787946acf0a6f0f89fb3a
codelifee <suminjung09@gmail.com>
2020-12-13 21:38:28 +0900

    Refined

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -6,24 +6,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <Counter /> */}
         <TodoApp />
       </div>
     );
   }
 }
-
-/* class LearningComponents extends Component {
-  render() {
-    return (
-      <div className='LearningComponents'>
-        My Hello World
-        <FirstComponent />
-        <SecondComponent />
-        <ThirdComponent />
-      </div>
-    );
-  }
-} */
-
 export default App;
